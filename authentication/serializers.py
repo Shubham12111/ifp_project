472fc219df9c9d5355bcc8c786898a39aac9a40a
aman <amanshitta@crossml.com>
2023-07-18 18:21:52 +0530

    Add:   - Added Signup and Forgot Password serializer.   - Added template for seignup page, forgot password page, verify OTP page.

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -37,3 +37,123 @@ class LoginSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('email', 'password')
+
+class SignupSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(
+        label=('Name *'),
+        max_length=100,
+        style={
+            "input_type": "text",
+            "autofocus": True,
+            "autocomplete": "off",
+            "required": True,
+        },
+        error_messages={
+            "required": "This field is required.",
+            "blank": "Name field cannot be blank.",
+        },
+    )
+    
+    email = serializers.EmailField(
+        label=('Email *'),
+        max_length=100,
+        style={
+            "input_type": "email",
+            "autofocus": True,
+            "autocomplete": "off",
+            "required": True,
+        },
+        error_messages={
+            "required": "This field is required.",
+            "blank": "Email field cannot be blank.",
+        },
+    )
+    password = serializers.CharField(
+        label=('Password *'),
+        max_length=100,
+        style={
+            "input_type": "password",
+            "autofocus": True,
+            "autocomplete": "off",
+            "required": True,
+        },
+        error_messages={
+            "required": "This field is required.",
+            "blank": "Password field cannot be blank.",
+        },
+    )
+    confirm_password = serializers.CharField(
+        label=('Confirm Password *'),
+        max_length=100,
+        style={
+            "input_type": "password",
+            "autofocus": True,
+            "autocomplete": "off",
+            "required": True,
+        },
+        error_messages={
+            "required": "This field is required.",
+            "blank": "Confirm Password field cannot be blank.",
+        },
+    )
+
+    def validate(self, data):
+        password = data.get('password')
+        confirm_password = data.get('confirm_password')
+
+        if password and confirm_password and password != confirm_password:
+            raise serializers.ValidationError({"confirm_password": "Passwords do not match."})
+
+        return data
+
+    def validate_email(self, value):
+        # Call the parent's is_valid() method to perform the default validationif 
+        if value:
+            user_email = User.objects.filter(email=value).exists()
+            if user_email:
+                raise serializers.ValidationError('User with this email already exists, Please try with differnt email.')
+        return value
+
+    class Meta:
+        model = User
+        fields = ('name', 'email', 'password', 'confirm_password')
+
+class ForgotPasswordSerializer(serializers.Serializer):
+    
+    email = serializers.EmailField(
+        label=('Email *'),
+        max_length=100,
+        style={
+            "input_type": "email",
+            "autofocus": True,
+            "autocomplete": "off",
+            "required": True,
+        },
+        error_messages={
+            "required": "This field is required.",
+            "blank": "Email field cannot be blank.",
+        },
+    )
+
+    class Meta:
+        fields = ('email')
+
+class VerifyOTPSerializer(serializers.Serializer):
+    
+    otp = serializers.CharField(
+        label=('OTP *'),
+        max_length=100,
+        style={
+            "input_type": "text",
+            "autofocus": True,
+            "autocomplete": "off",
+            "required": True,
+        },
+        error_messages={
+            "required": "This field is required.",
+            "blank": "OTP cannot be blank.",
+        },
+    )
+
+    class Meta:
+        fields = ('otp')
--- authentication/views.py
+++ authentication/views.py
@@ -5,8 +5,8 @@ from rest_framework import status
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 from django.contrib import messages
-
-from .serializers import LoginSerializer
+from django.shortcuts import redirect
+from .serializers import LoginSerializer, SignupSerializer, ForgotPasswordSerializer, VerifyOTPSerializer
 from infinity_fire_solutions.response_schemas import create_api_response
 
 class LoginView(APIView):
@@ -23,22 +23,16 @@ class LoginView(APIView):
         """
         return Response({'serializer': serializer}, template_name=self.template_name)
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request):
         """
         Handle GET request for login page.
         """
         serializer = self.serializer_class()
 
-        if request.accepted_renderer.format == 'html':
-            # Render the HTML template for login page
-            return self.render_html_response(serializer)
-        else:
-            # Return JSON response with error message
-            return create_api_response(status_code=status.HTTP_400_BAD_REQUEST,
-                                       message="Please ensure that the entered details are correct and try again.",
-                                       data=serializer.data)
+        # Render the HTML template for login page
+        return self.render_html_response(serializer)
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         """
         Handle POST request for user login.
         """
@@ -54,7 +48,7 @@ class LoginView(APIView):
             if not user:
                 # User authentication failed
                 # coming from the webiste TODO
-                if request.accepted_renderer.format == 'html' and 'HTTP_USER_AGENT' in request.META:
+                if request.accepted_renderer.format == 'html':
                     # Render the HTML template with error message
                     messages.error(request, 'Login failed. The credentials provided are incorrect. Please verify your login information and try again.')
                     return self.render_html_response(serializer)
@@ -68,7 +62,7 @@ class LoginView(APIView):
             user = user
             token = Token.objects.get_or_create(user=user)
 
-            if request.accepted_renderer.format == 'html' and 'HTTP_USER_AGENT' in request.META:
+            if request.accepted_renderer.format == 'html':
                 # Render the HTML template for successful login
                 return self.render_html_response(serializer)
             else:
@@ -87,3 +81,173 @@ class LoginView(APIView):
                 return create_api_response(status_code=status.HTTP_400_BAD_REQUEST,
                                             message="Please ensure that the entered details are correct and try again.",
                                             data=serializer.errors)
+
+class SignupView(APIView):
+    renderer_classes = [TemplateHTMLRenderer, JSONRenderer]
+    serializer_class = SignupSerializer
+    template_name = "signup.html"
+
+    
+    def render_html_response(self, serializer):
+        """
+        Render HTML response using the provided serializer and template name.
+        """
+        return Response({'serializer': serializer}, template_name=self.template_name)
+
+    def get(self, request):
+        """
+        Handle GET request for login page.
+        """
+        serializer = self.serializer_class()
+
+        # Render the HTML template for login page
+        return self.render_html_response(serializer)
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            # Validating the serializer data
+            name = serializer.validated_data.get('name')
+            email = serializer.validated_data.get('email')
+            password = serializer.validated_data.get('password')
+
+            register_data = {
+                'name':  name,
+                'email': email,
+                'password': password
+            }
+            # Create a new user with the validated serializer data
+            user_serializer = LoginSerializer(data=register_data)
+            if user_serializer.is_valid():
+                try:
+                    user = user_serializer.save()
+                except Exception as e:
+                    error = str(e)
+                    messages.error(request, f'{error}')
+
+                    if request.accepted_renderer.format == 'html':
+                        # Render the HTML template for successful signup
+                        return self.render_html_response(serializer)
+                    else:
+                        # Return JSON response with success message or relevant data
+                        return create_api_response(status_code=status.HTTP_201_CREATED,
+                                            message="User signed up successfully.",
+                                            data={'error': error})
+                        
+                # Additional logic after user signup (e.g., sending email, generating tokens, etc.)
+                messages.success(request, 'User signed up successfully.')
+                
+                if request.accepted_renderer.format == 'html':
+                    # Render the HTML template for successful signup
+                    return redirect('/auth/login/')
+                else:
+                    # Return JSON response with success message or relevant data
+                    return create_api_response(status_code=status.HTTP_201_CREATED,
+                                            message="User signed up successfully.",
+                                            data={'user_id': user.id})
+            else:
+                messages.error(request, 'User Registeration Failed.')
+                return self.render_html_response(serializer)
+
+        else:
+            # Invalid serializer data
+            if request.accepted_renderer.format == 'html':
+                # Render the HTML template with invalid serializer data
+                return self.render_html_response(serializer)
+            else:
+                # Return JSON response with error message
+                return create_api_response(status_code=status.HTTP_400_BAD_REQUEST,
+                                           message="Please ensure that the entered details are correct and try again.",
+                                           data=serializer.errors)
+
+
+class ForgotPasswordView(APIView):
+    renderer_classes = [TemplateHTMLRenderer, JSONRenderer]
+    serializer_class = ForgotPasswordSerializer
+    template_name = "forgot.html"
+
+    
+    def render_html_response(self, serializer):
+        """
+        Render HTML response using the provided serializer and template name.
+        """
+        return Response({'serializer': serializer}, template_name=self.template_name)
+
+    def get(self, request):
+        """
+        Handle GET request for login page.
+        """
+        serializer = self.serializer_class()
+
+        # Render the HTML template for login page
+        return self.render_html_response(serializer)
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            if request.accepted_renderer.format == 'html':
+                # Render the HTML template with invalid serializer data
+                messages.info(request, "OTP will be sent to your email, if you're registered.")
+                return redirect('/auth/verify/')
+            else:
+                # Return JSON response with error message
+                return create_api_response(status_code=status.HTTP_400_BAD_REQUEST,
+                                           message="OTP will be sent to your email, if you're registered.",
+                                           data=serializer.errors)
+        else:
+            # Invalid serializer data
+            if request.accepted_renderer.format == 'html':
+                # Render the HTML template with invalid serializer data
+                return self.render_html_response(serializer)
+            else:
+                # Return JSON response with error message
+                return create_api_response(status_code=status.HTTP_400_BAD_REQUEST,
+                                           message="Please ensure that the entered details are correct and try again.",
+                                           data=serializer.errors)
+
+class VerifyOTPView(APIView):
+    renderer_classes = [TemplateHTMLRenderer, JSONRenderer]
+    serializer_class = VerifyOTPSerializer
+    template_name = "verify_otp.html"
+
+    
+    def render_html_response(self, serializer):
+        """
+        Render HTML response using the provided serializer and template name.
+        """
+        return Response({'serializer': serializer}, template_name=self.template_name)
+
+    def get(self, request):
+        """
+        Handle GET request for login page.
+        """
+        serializer = self.serializer_class()
+
+        # Render the HTML template for login page
+        return self.render_html_response(serializer)
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            if request.accepted_renderer.format == 'html':
+                # Render the HTML template with invalid serializer data
+                messages.warning(request, "Not Implemented yet")
+                return self.render_html_response(serializer)
+            else:
+                # Return JSON response with error message
+                return create_api_response(status_code=status.HTTP_400_BAD_REQUEST,
+                                            message="Not Implemented yet",
+                )
+        else:
+            # Invalid serializer data
+            if request.accepted_renderer.format == 'html':
+                # Render the HTML template with invalid serializer data
+                return self.render_html_response(serializer)
+            else:
+                # Return JSON response with error message
+                return create_api_response(status_code=status.HTTP_400_BAD_REQUEST,
+                                           message="Please ensure that the entered details are correct and try again.",
+                                           data=serializer.errors)
