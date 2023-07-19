6096b0be062213911655644e64609651756c51f1
shubham <shubham@shubham-B360M-GAMING-HD>
2023-07-19 12:57:32 +0530

    1.Ui Screen to show conversation history in a cronological order

---
+++ contact/urls.py
@@ -0,0 +1,7 @@
+# urls.py
+from django.urls import path
+from .views import MessageListView
+
+urlpatterns = [
+    path('messages/', MessageListView.as_view(), name='messages'),
+]
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,11 @@
+# views.py
 from django.shortcuts import render
+from django.views import View
+from .models import *
 
-# Create your views here.
+class MessageListView(View):
+    template_name = 'messages.html'
+
+    def get(self, request):
+        messages = Contact.objects.all()
+        return render(request, self.template_name, {'messages': messages})
