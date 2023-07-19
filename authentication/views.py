b81b5c78e6b2475ce6c32bef5fb3f38b3cfb8685
aman <amanshitta@crossml.com>
2023-07-19 12:39:14 +0530

    removed API view from auth module

--- authentication/views.py
+++ authentication/views.py
@@ -48,42 +48,18 @@ class LoginView(APIView):
 
             if not user:
                 # User authentication failed
-                # coming from the webiste TODO
-                if request.accepted_renderer.format == 'html':
-                    # Render the HTML template with error message
-                    messages.error(request, 'Login failed. The credentials provided are incorrect. Please verify your login information and try again.')
-                    return self.render_html_response(serializer)
-                else:
-                    # Return JSON response with error message
-                    return create_api_response(status_code=status.HTTP_400_BAD_REQUEST,
-                                                message="Please ensure that the entered details are correct and try again.",
-                                                data=serializer.errors)
+                # Render the HTML template with error message
+                messages.error(request, 'Login failed. The credentials provided are incorrect. Please verify your login information and try again.')
+                return self.render_html_response(serializer)
 
-            # User authentication successful
-            user = user
-            token = Token.objects.get_or_create(user=user)
-
-            if request.accepted_renderer.format == 'html':
-                # Render the HTML template for successful login
-                messages.success(request, 'Login failed. The credentials provided are incorrect. Please verify your login information and try again.')
-                return redirect(reverse('dashboard'))
-                #return self.render_html_response(serializer)
-            else:
-                # Return JSON response with token
-                return create_api_response(status_code=status.HTTP_200_OK,
-                                            message="Please ensure that the entered details are correct and try again.",
-                                            data={'token':token[0].key})
+            # Render the HTML template for successful login
+            messages.success(request, 'Login failed. The credentials provided are incorrect. Please verify your login information and try again.')
+            return redirect(reverse('dashboard'))
 
         else:
-            # Invalid serializer data
-            if request.accepted_renderer.format == 'html':
-                # Render the HTML template with invalid serializer data
-                return self.render_html_response(serializer)
-            else:
-                # Return JSON response with error message
-                return create_api_response(status_code=status.HTTP_400_BAD_REQUEST,
-                                            message="Please ensure that the entered details are correct and try again.",
-                                            data=serializer.errors)
+            # Render the HTML template with invalid serializer data
+            return self.render_html_response(serializer)
+
 
 class SignupView(APIView):
     renderer_classes = [TemplateHTMLRenderer, JSONRenderer]
@@ -120,6 +96,7 @@ class SignupView(APIView):
                 'email': email,
                 'password': password
             }
+
             # Create a new user with the validated serializer data
             user_serializer = LoginSerializer(data=register_data)
             if user_serializer.is_valid():
@@ -129,40 +106,21 @@ class SignupView(APIView):
                     error = str(e)
                     messages.error(request, f'{error}')
 
-                    if request.accepted_renderer.format == 'html':
-                        # Render the HTML template for successful signup
-                        return self.render_html_response(serializer)
-                    else:
-                        # Return JSON response with success message or relevant data
-                        return create_api_response(status_code=status.HTTP_201_CREATED,
-                                            message="User signed up successfully.",
-                                            data={'error': error})
+                    # Render the HTML template for successful signup
+                    return self.render_html_response(serializer)
                         
                 # Additional logic after user signup (e.g., sending email, generating tokens, etc.)
                 messages.success(request, 'User signed up successfully.')
-                
-                if request.accepted_renderer.format == 'html':
-                    # Render the HTML template for successful signup
-                    return redirect('/auth/login/')
-                else:
-                    # Return JSON response with success message or relevant data
-                    return create_api_response(status_code=status.HTTP_201_CREATED,
-                                            message="User signed up successfully.",
-                                            data={'user_id': user.id})
+
+                # Render the HTML template for successful signup
+                return redirect('/auth/login/')
             else:
                 messages.error(request, 'User Registeration Failed.')
                 return self.render_html_response(serializer)
 
         else:
-            # Invalid serializer data
-            if request.accepted_renderer.format == 'html':
-                # Render the HTML template with invalid serializer data
-                return self.render_html_response(serializer)
-            else:
-                # Return JSON response with error message
-                return create_api_response(status_code=status.HTTP_400_BAD_REQUEST,
-                                           message="Please ensure that the entered details are correct and try again.",
-                                           data=serializer.errors)
+            # Render the HTML template with invalid serializer data
+            return self.render_html_response(serializer)
 
 
 class ForgotPasswordView(APIView):
@@ -190,25 +148,15 @@ class ForgotPasswordView(APIView):
         serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
-            if request.accepted_renderer.format == 'html':
-                # Render the HTML template with invalid serializer data
-                messages.info(request, "OTP will be sent to your email, if you're registered.")
-                return redirect('/auth/verify/')
-            else:
-                # Return JSON response with error message
-                return create_api_response(status_code=status.HTTP_400_BAD_REQUEST,
-                                           message="OTP will be sent to your email, if you're registered.",
-                                           data=serializer.errors)
+            # Render the HTML template with invalid serializer data
+            messages.info(request, "OTP will be sent to your email, if you're registered.")
+            return redirect('/auth/verify/')
+
         else:
             # Invalid serializer data
-            if request.accepted_renderer.format == 'html':
-                # Render the HTML template with invalid serializer data
-                return self.render_html_response(serializer)
-            else:
-                # Return JSON response with error message
-                return create_api_response(status_code=status.HTTP_400_BAD_REQUEST,
-                                           message="Please ensure that the entered details are correct and try again.",
-                                           data=serializer.errors)
+            # Render the HTML template with invalid serializer data
+            return self.render_html_response(serializer)
+
 
 class VerifyOTPView(APIView):
     renderer_classes = [TemplateHTMLRenderer, JSONRenderer]
@@ -235,22 +183,11 @@ class VerifyOTPView(APIView):
         serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
-            if request.accepted_renderer.format == 'html':
-                # Render the HTML template with invalid serializer data
-                messages.warning(request, "Not Implemented yet")
-                return self.render_html_response(serializer)
-            else:
-                # Return JSON response with error message
-                return create_api_response(status_code=status.HTTP_400_BAD_REQUEST,
-                                            message="Not Implemented yet",
-                )
+            # Render the HTML template with invalid serializer data
+            messages.warning(request, "Not Implemented yet")
+            return self.render_html_response(serializer)
+
         else:
             # Invalid serializer data
-            if request.accepted_renderer.format == 'html':
-                # Render the HTML template with invalid serializer data
-                return self.render_html_response(serializer)
-            else:
-                # Return JSON response with error message
-                return create_api_response(status_code=status.HTTP_400_BAD_REQUEST,
-                                           message="Please ensure that the entered details are correct and try again.",
-                                           data=serializer.errors)
+            # Render the HTML template with invalid serializer data
+            return self.render_html_response(serializer)
