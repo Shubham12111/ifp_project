da4475a235072bc9d159375aae45d5cf2cbdaf3e
aman <amanshitta@crossml.com>
2023-07-21 16:29:51 +0530

    Added required to serializer field and Create, Uupdate and Get view for todod added

--- todo/views.py
+++ todo/views.py
@@ -4,6 +4,11 @@ from rest_framework.response import Response
 from rest_framework.renderers import TemplateHTMLRenderer,JSONRenderer
 from infinity_fire_solutions.response_schemas import create_api_response
 from .models import *
+from .serializers import *
+from django.contrib import messages 
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.shortcuts import get_object_or_404
 
 class ToDoListView(generics.ListAPIView):
     """ view to get the listing of all contacts
@@ -17,6 +22,11 @@ class ToDoListView(generics.ListAPIView):
         if request.accepted_renderer.format == 'html':
             # If the client accepts HTML, render the template
             return Response({'todo_list': queryset}, template_name=self.template_name)
+        else:
+            serializer = self.serializer_class(queryset, many=True)
+            return create_api_response(status_code=status.HTTP_200_OK,
+                                       message="Data Retrieved successfully",
+                                       data=serializer)
         
         # # If the client accepts JSON (DataTable's request), handle server-side processing
         # draw = int(request.GET.get('draw', 1))
@@ -39,4 +49,63 @@ class ToDoListView(generics.ListAPIView):
         #     'recordsFiltered': total_records,
         #     'data': serializer.data
         # })
+
+class ToDoAddView(generics.CreateAPIView):
     
+    renderer_classes = [TemplateHTMLRenderer, JSONRenderer]
+    template_name = 'todo_add.html'
+    serializer_class = TodoAddSerializer
+
+    def render_html_response(self, serializer):
+        """
+        Render HTML response using the provided serializer and template name.
+        """
+        return Response({'serializer': serializer}, template_name=self.template_name)
+    
+        
+    def get(self, request, *args, **kwargs):
+        
+        if kwargs.get('pk'):
+
+            # If a primary key is provided, it means we are editing an existing contact
+            data = get_object_or_404(Todo, pk=kwargs.get('pk'), user_id=request.user)
+            serializer = self.serializer_class(instance=data)
+        else:
+            # If no primary key is provided, it means we are adding a new contact
+            serializer = self.serializer_class()
+
+        if request.accepted_renderer.format == 'html':
+            # If the client accepts HTML, render the template
+            return self.render_html_response(serializer)
+    
+    def post(self, request, *args, **kwargs):
+
+        data = request.data
+
+        if kwargs.get('pk'):
+            # If a primary key is provided, it means we are editing an existing contact
+            todo_data = get_object_or_404(Todo, pk=kwargs.get('pk'), user_id=request.user)
+            serializer = self.serializer_class(instance=todo_data, data=data)
+        else:
+            # If no primary key is provided, it means we are adding a new contact
+            serializer = self.serializer_class(data=data)
+
+        
+
+        serializer = self.serializer_class(data=data)
+
+        if serializer.is_valid():
+            serializer.validated_data['user_id'] = request.user 
+            serializer.save()
+            if request.accepted_renderer.format == 'html':
+                messages.success(request, f"Task Added: Your TODO have been saved successfully! ")
+                return redirect(reverse('todo_list'))
+            else:
+                return create_api_response(status_code=status.HTTP_200_OK,
+                                           message="Data retrived succefully",
+                                           data=serializer.data
+                                           )
+        else:
+            # Render the HTML template with invalid serializer data
+            return self.render_html_response(serializer)
+
