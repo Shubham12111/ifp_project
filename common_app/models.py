5ec0f27a3e19c5cc627e1e64c40c03ae583f1a2f
payal <payal@crossml.com>
2023-08-08 22:56:47 +0530

    Support to the EmailTemplates

--- common_app/models.py
+++ common_app/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import Group
+from ckeditor.fields import RichTextField
 
+PURPOSE_CHOICES = [
+        ('new_user_registration', 'New User Registration'),
+ ]    
 class MenuItem(models.Model):
     permission_required = models.BooleanField(default=False)
     name = models.CharField(max_length=100)
@@ -13,3 +17,16 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class EmailNotificationTemplate(models.Model):
+    purpose = models.CharField(max_length=50, choices=PURPOSE_CHOICES, unique=True)
+    subject = models.CharField(max_length=200)
+    recipient = models.EmailField()
+    content =  RichTextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True,null=True)
+
+    
+    def __str__(self):
+        return self.subject
