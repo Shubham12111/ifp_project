e60cad760862db4955c1309a8220c9c122393268
payal <payal@crossml.com>
2023-08-21 01:18:20 +0530

    Able to delete the converstion

--- contact/views.py
+++ contact/views.py
@@ -551,7 +551,7 @@ class ConversationRemoveDocumentView(generics.DestroyAPIView):
         Get the queryset of contacts filtered by the current user.
         """
         user_id = self.request.user.id
-        return Contact.objects.filter(pk=self.kwargs.get('contact_id'), user_id=user_id).get()
+        return Contact.objects.filter(pk=self.kwargs.get('contact_id')).get()
     
     def destroy(self, request, *args, **kwargs):
         """
