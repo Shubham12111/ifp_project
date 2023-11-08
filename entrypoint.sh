971954ed6703af04b3aa95b2634af8363c78c8fd
Aman Shitta <88380453+amanshitta-crossml@users.noreply.github.com>
2023-11-08 11:03:03 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 echo "[+] ------ Apply database migrations ------ [+]"
 
 # python3 manage.py makemigrations --merge --no-input 
-python3 manage.py migrate work_planning_management
+python3 manage.py migrate 
+
 # echo "[+] ------ Populating Cities Light Data ------ [+]"
 # python3 manage.py cities_light
 
