45a10b200c101a8c217be97d11c2b4ff0b5cf503
Shubham-crossml <shubham.sharma@crossml.com>
2023-12-29 15:26:39 +0530

    removed unwanted code

--- authentication/middleware.py
+++ authentication/middleware.py
@@ -123,20 +123,6 @@ class ForcePasswordChangeBackend:
         response = self.get_response(request)
         return response
 
-class CheckAdminUserMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        # Exclude admin URLs from superuser access enforcement
-        if not request.path.startswith('/admin/'):
-            if request.user.is_authenticated and request.user.is_superuser:
-                # If the user is authenticated and is a superuser, log them out and redirect to the login page.
-                logout(request)
-                return redirect('login')
-
-        response = self.get_response(request)
-        return response    
 class SimpleAPILoggerMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
