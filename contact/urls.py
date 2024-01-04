999feb6f709247aec262b69e5ef5b2b5055da018
Anjali-crossml <anjali.rana@crossml.com>
2024-01-04 18:42:05 +0530

    add serachable dropdown and task module changes

--- contact/urls.py
+++ contact/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('conversation/edit/<int:contact_id>/<int:conversation_id>', login_required(ConversationView.as_view()), name='edit_conversation'),
     path('conversation/delete/<int:contact_id>/<int:conversation_id>', login_required(ConversationCommentView.as_view()), name='delete_conversation'),
     path('conversation/delete/document/<int:contact_id>/<int:conversation_id>', login_required(ConversationRemoveDocumentView.as_view()), name='document_conversation'),
-    # path('autocomplete_contact/', ContacttypeAutocomplete.as_view(), name='contact-autocomplete'),
+    path('autocomplete_contact/', ContacttypeAutocomplete.as_view(), name='contact-autocomplete'),
     # path('autocomplete_conversation/', ConversationtypeAutocomplete.as_view(), name='conversation-autocomplete'),
     # path('autocomplete_postcode/', PostcodeChoicesAutocomplete.as_view(), name='postcode-autocomplete'),
     
